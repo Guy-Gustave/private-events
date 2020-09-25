c2b64eb14a821dfb183ea2a46fc71c5077b50adb
Guy <guymoustapha@gmail.com>
2020-09-25 04:08:03 +0200

    add description to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,53 @@
 class EventsController < ApplicationController
+  class EventsController < ApplicationController
+
+    def index
+      @events= Event.all
+    end
+  
+    def show
+      @event = Event.find(params[:id])
+    end
+
+    def new
+      @event = Event.new
+    end
+    
+    def  create
+      
+      @event = current_user.events.build(event_params)
+      @event.creator_id = session[:user_id]
+      if @event.save
+        flash[:success] = "Event successfully created"
+        redirect_to event_path(@event)
+      else
+
+        flash[:error] = "Something went wrong"
+        render 'new'
+      end
+    end
+
+    # def register
+    #   @attendance = Attendance.create(event_id: params[:id], user_id: session[:user_id])
+    #   if @attendance.save
+    #     flash[:notice] = 'welcome to the event, Registered sucessfully'
+    #     redirect_to event_path
+    #   end
+    # end
+   
+  
+    # def update
+    #   @ = .find(params[:id])
+    # end
+  
+    # def edit
+    #   @ = .find(params[:id])
+    # end
+  
+    # def destroy
+    #    = .find(params[:id])
+    # end
+  
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_015629) do
+ActiveRecord::Schema.define(version: 2020_09_25_020632) do
 
   create_table "events", force: :cascade do |t|
     t.date "event_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
