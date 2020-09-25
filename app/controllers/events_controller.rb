3f4944d399118b08acbfa582af29a663b4ed6c05
Guy <guymoustapha@gmail.com>
2020-09-25 04:37:35 +0200

    add views event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,53 +1,54 @@
 class EventsController < ApplicationController
-  class EventsController < ApplicationController
 
-    def index
-      @events= Event.all
-    end
+  def index
+    @events= Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
   
-    def show
-      @event = Event.find(params[:id])
-    end
-
-    def new
-      @event = Event.new
-    end
+  def  create
     
-    def  create
-      
-      @event = current_user.events.build(event_params)
-      @event.creator_id = session[:user_id]
-      if @event.save
-        flash[:success] = "Event successfully created"
-        redirect_to event_path(@event)
-      else
+    @event = current_user.events.build(event_params)
+    @event.creator_id = session[:user_id]
+    if @event.save
+      flash[:success] = "Event successfully created"
+      redirect_to event_path(@event)
+    else
 
-        flash[:error] = "Something went wrong"
-        render 'new'
-      end
+      flash[:error] = "Something went wrong"
+      render 'new'
     end
+  end
 
-    # def register
-    #   @attendance = Attendance.create(event_id: params[:id], user_id: session[:user_id])
-    #   if @attendance.save
-    #     flash[:notice] = 'welcome to the event, Registered sucessfully'
-    #     redirect_to event_path
-    #   end
-    # end
-   
-  
-    # def update
-    #   @ = .find(params[:id])
-    # end
-  
-    # def edit
-    #   @ = .find(params[:id])
-    # end
-  
-    # def destroy
-    #    = .find(params[:id])
-    # end
-  
-end
+  # def register
+  #   @attendance = Attendance.create(event_id: params[:id], user_id: session[:user_id])
+  #   if @attendance.save
+  #     flash[:notice] = 'welcome to the event, Registered sucessfully'
+  #     redirect_to event_path
+  #   end
+  # end
+ 
 
+  # def update
+  #   @ = .find(params[:id])
+  # end
+
+  # def edit
+  #   @ = .find(params[:id])
+  # end
+
+  # def destroy
+  #    = .find(params[:id])
+  # end
+  private
+
+  def event_params
+    params.require(:event).permit(:event_date, :description)
+  end
 end
